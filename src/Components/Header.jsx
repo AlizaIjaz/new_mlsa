a2f0cfbca11a660d57cbd9a4e033123deadd99dd
Aliza Ijaz <104016478+AlizaIjaz@users.noreply.github.com>
2024-05-16 10:54:25 +0000

    firstchange

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,32 +1,52 @@
 /**
- * Header component
+ * Home component
  *
- * Top navigation bar for your site. Set to remain visible as the
- * user scrolls so that they can constantly reach any part of your page.
+ * The section at the top of the page to display image of your
+ * choice, name and title that describes your career focus.
  */
-import React from "react";
 
-const Header = () => {
+import React from "react";
+import arrowSvg from "../images/code.jpg";
+import PropTypes from "prop-types";
+
+/**
+ * Home background image
+ *
+ * Below is a sample image. Upload the image of your choice into the "images"
+ * directory and import here for use. Then, set imageAltText to string that 
+ * represents what you see in that image.
+ *
+ *
+ * Need an image? Check out https://unsplash.com to download a photo you
+ * freely use on your site.
+ */
+import image from "../images/woman-with-tablet.jpg";
+
+const imageAltText = "Aliza ijaz -Web Developer And Android Developer- This is my background photo";
+
+const Home = ({ name, title }) => {
   return (
-    <div
-      style={{
-        position: "fixed",
-        display: "flex",
-        justifyContent: "center",
-        gap: "2rem",
-        background: "rgba(255,255,255,0.75)",
-        padding: "1rem",
-        top: 0,
-        width: "100%",
-        zIndex: 10,
-      }}
-    >
-      <a href="#home">Home</a>
-      <a href="#about">About</a>
-      <a href="#portfolio">Portfolio</a>
-      <a href="#footer">Contact</a>
-    </div>
+    <section id="home" className="min-height">
+      <img className="background" src={image} alt="" />
+      <div style={{ position: "absolute", top: "5rem", left: "2rem", width: "17rem" }}>
+        <h1>{name}</h1>
+        <h2>{title}</h2>
+      </div>
+      <div style={{ position: "absolute", bottom: "3rem", left: "50%" }}>
+        <img src={arrowSvg} style={{ height: "3rem", width: "3rem" }} alt={imageAltText} />
+      </div>
+    </section>
   );
 };
 
-export default Header;
+Home.defaultProps = {
+  name: "",
+  title: "",
+};
+
+Home.propTypes = {
+  name: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+};
+
+export default Home;
--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -19,9 +19,9 @@ import React from "react";
  * Need an image? Check out https://unsplash.com to download a photo you
  * freely use on your site.
  */
-import image from "../images/design-desk.jpeg";
+import image from "../images/binary.jpg";
 
-const imageAltText = "desktop with books and laptop";
+const imageAltText = "binary code background";
 
 /**
  * Project list
@@ -31,28 +31,28 @@ const imageAltText = "desktop with books and laptop";
  */
 const projectList = [
   {
-    title: "10 Things To Know About Azure Static Web Apps 🎉",
+    title: "Music App created by me 🎉",
     description:
-      "Collaboration to create a beginner friendly article to help explain Azure Static Web Apps and tooling to get started.",
-    url: "https://dev.to/azure/10-things-to-know-about-azure-static-web-apps-3n4i",
+      "Created a user friendly music player app.",
+    url: "https://github.com/AlizaIjaz/Music-Player",
   },
   {
-    title: "Web Development for Beginners",
+    title: "PDF viewer Application",
     description:
-      "Contributed sketch note imagery to accompany each lesson. These help provide visual representation of what is being taught.",
-    url: "https://github.com/microsoft/web-dev-for-beginners",
+      "Created a Pdf viewer application to view the pdf documents.",
+    url: "https://github.com/AlizaIjaz/pdf_viewer",
   },
   {
-    title: "My Resume Site",
+    title: "House Price prediction ",
     description:
-      "Created from Microsoft's resume workshop and deployed to GitHub pages. Includes my experience and design abilities.",
-    url: "https://github.com/microsoft/workshop-library/tree/main/full/build-resume-website",
+      "Perform house price prediction using Linear Regression.",
+    url: "https://github.com/AlizaIjaz/House-Price-Prediction-",
   },
   {
-    title: "GitHub Codespaces and github.dev",
+    title: "Vine Quality Prediction",
     description:
-      "Video interview to explain when to use GitHub.dev versus GitHub Codespaces, and how best to use each tool.",
-    url: "https://www.youtube.com/watch?v=c3hHhRME_XI",
+      "Predict the quality of Vine using Linear regression.",
+    url: "https://github.com/AlizaIjaz/Wine-Quality-Prediction",
   },
 ];
 
